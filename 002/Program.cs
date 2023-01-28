feb0daf0437f1006094f1f3cddacd2a03721d9ae
Rustam <rustamzakirov@mail.ru>
2023-01-29 00:49:22 +0300

    second complete

--- 002/Program.cs
+++ 002/Program.cs
@@ -3,3 +3,17 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+int max;
+if (a < b) 
+    max = b;
+else
+    max = a;
+if (max < c)
+    max = c;
+Console.WriteLine($"максимальное число = {max}");
